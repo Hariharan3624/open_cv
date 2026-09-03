9ceb1ddab4ffad1e98bc7bf18c40219237651965
Hariharan3624 <vikrammanoharan@gmail.com>
2026-09-03 18:24:14 +0100

    Update basic_image_manupulation.py

--- basic_image_manupulation.py
+++ basic_image_manupulation.py
@@ -1,20 +1,28 @@
 import cv2
 import matplotlib.pyplot as plt
-from PIL import Image
+
 image = cv2.imread('example.jpg')
-image = Image.open('example.jpg')
-rotated_image = image.rotate(45, expand=True)
-rotated_image.save('rotated_pillow.jpg')
+
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.imshow(image_rgb)
 plt.title("RGB Image")
 plt.show
+
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray_image, cmap='gray')
 plt.title("Gayscale Image")
 plt.show()
+
 cropped_image = image[100:300, 200:400]
 cropped_rgb = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
 plt.imshow(cropped_rgb)
 plt.title("Cropped Region")
 plt.show()
+key = cv2.waitKey(0)
+if key == ord('s'):
+    cv2.imwrite('grayscale_resized_image.jpg', cropped_image)
+    print("Image saved as grayscale_resized_image.jpg")
+else:
+    print("Image not save")
+cv2.destroyAllWindows()
+print(f"Processed Image Dimensions: {cropped_image.shape}")
